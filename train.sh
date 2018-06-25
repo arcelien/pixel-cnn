91a1174d44698a5b920504095ee74c28705402ef
Daniel Ho <danho@nvidia.com>
2018-06-25 12:48:57 -0700

    updating gitignore and run script

--- train.sh
+++ train.sh
@@ -1,1 +1,3 @@
-python train.py --save_dir ../HAWC/saves --data_dir ../HAWC/data --save_interval 3 --data_set hawc2
+python train.py \
+ --nosample --save_dir ../HAWC/saves --data_dir ../HAWC/data \
+ --save_interval 3 --data_set hawc2
